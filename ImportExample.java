685a9745f38575a777af99733769820a69b98495
julio <juliodisouza@gmail.com>
2019-07-01 17:24:00 -0300

    commit inicial

---
+++ ImportExample.java
@@ -0,0 +1,9 @@
+//import java.util.Random;
+import java.util.*;
+
+public class ImportExample {
+	public static void main(String[] args) {
+		Random r = new Random(); // DOES NOT COMPILE
+		System.out.println(r.nextInt(10));
+	}
+}
