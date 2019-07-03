cce132ca560e9c86849d971955bdfe0e0389d385
julio <juliodisouza@gmail.com>
2019-07-03 17:41:49 -0300

    chapter 2 - Operators

---
+++ Arithimetic.java
@@ -0,0 +1,12 @@
+public class Arithimetic{
+	public static void main(String... args){
+		System.out.println(9/3);
+		System.out.println(9 % 3); // Outputs 0
+		System.out.println(10 / 3); // Outputs 3
+		System.out.println(10 % 3); // Outputs 1
+		System.out.println(11 / 3); // Outputs 3
+		System.out.println(11 % 3); // Outputs 2
+		System.out.println(12 / 3); // Outputs 4
+		System.out.println(12 % 3); // Outputs 0
+	}
+}
