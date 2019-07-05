a2757dcc77a1950182c0e3027b7d301fc2c232dd
julio <juliodisouza@gmail.com>
2019-07-05 17:40:31 -0300

    Alguns testes

--- Test.java
+++ Test.java
@@ -1,6 +1,47 @@
+import java.util.*;
+
 public class Test {
+	
 	public static void main(String[] args){
-		int num = 1_000_000;
-		System.out.println(num);
+		
+		java.util.List<String> lst = new java.util.ArrayList<String>();
+		lst.add("Neymar");
+		lst.add("Griezmann");
+		lst.add("Messi");
+		
+		/*
+		String[] arr = new String[3];
+		arr[0] = "Julio";
+		arr[1] = "Julia";
+		arr[2] = "Ajax";
+		*/
+		
+		/*
+		for (String n : arr){
+			System.out.print(n + " ");
+		}
+		*/
+		
+		/*
+		for (int i = 0; i < arr.length; i++){
+			System.out.println(arr[i]);
+		}
+		*/
+		
+		/*
+		for (String l : lst){
+			System.out.print(l + " ");
+		}		
+		*/
+		
+		for (int i = 0; i < lst.size(); i++){
+			String name = lst.get(i);
+			
+			System.out.print(name + " ");
+		}
+		
+		
 	}
+
 }
+
